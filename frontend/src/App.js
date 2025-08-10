dd2d423a2c2dbe65507f7c91f422741ca4734020
harlankilstein <113718821+harlankilstein@users.noreply.github.com>
2025-08-10 11:24:56 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './App.css';
+import { AuthProvider } from './AuthContext';
+import { StripeProvider } from './StripeContext';
 import AIWritingDetector from './AIWritingDetector';
 
 function App() {
   return (
-    <div className="App">
-      <AIWritingDetector />
-    </div>
+    <AuthProvider>
+      <StripeProvider>
+        <div className="App">
+          <AIWritingDetector />
+        </div>
+      </StripeProvider>
+    </AuthProvider>
   );
 }
 
